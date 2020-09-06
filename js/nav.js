26f32d58a83ffd178bf666da88ceefa7755b3b53
kapilan <kapilansrikaran@gmail.com>
2020-09-06 17:46:30 +0530

    Responsive-Navbar

---
+++ js/nav.js
@@ -0,0 +1,28 @@
+// define all variable
+const navToggler = document.querySelector(".nav-toggler");
+const navMenu = document.querySelector(".nav-list");
+const navLinks = document.querySelectorAll(".nav-link");
+
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener("click", togglerClick);
+  // nav links click event
+  navLinks.forEach((elem) => elem.addEventListener("click", navLinkClick));
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle("toggler-open");
+  navMenu.classList.toggle("open");
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if (navMenu.classList.contains("open")) {
+    navToggler.click();
+  }
+}
